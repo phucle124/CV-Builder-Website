03a36e5647831cfaa5e3ef19e07146ece4c84aa8
phucle124 <52844236+phucle124@users.noreply.github.com>
2025-12-23 11:20:41 +0700

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -22,37 +22,13 @@ app.use(express.json());
 const authRoutes = require('./routes/auth');
 const usersRoutes = require('./routes/users');
 const cvsRoutes = require('./routes/cvs');
-const awardsRoutes = require('./routes/awards');
-const certificatesRoutes = require('./routes/certificates');
-const contactsRoutes = require('./routes/contacts');
-const educationRoutes = require('./routes/education');
-const experienceRoutes = require('./routes/experience');
-const expBulletsRoutes = require('./routes/expBullets');
-const headersRoutes = require('./routes/headers');
-const highlightsRoutes = require('./routes/highlights');
-const highlightBulletsRoutes = require('./routes/highlightBullets');
-const languagesRoutes = require('./routes/languages');
-const projectsRoutes = require('./routes/projects');
-const projectResponsibilitiesRoutes = require('./routes/projectResponsibilities');
-const skillsRoutes = require('./routes/skills');
+
 
 // Mount routes
 app.use('/api', authRoutes);
 app.use('/api', usersRoutes);
 app.use('/api/cvs', cvsRoutes);
-app.use('/api/awards', awardsRoutes);
-app.use('/api/certificates', certificatesRoutes);
-app.use('/api/contacts', contactsRoutes);
-app.use('/api/education', educationRoutes);
-app.use('/api/experience', experienceRoutes);
-app.use('/api/exp-bullets', expBulletsRoutes);
-app.use('/api/headers', headersRoutes);
-app.use('/api/highlights', highlightsRoutes);
-app.use('/api/highlight-bullets', highlightBulletsRoutes);
-app.use('/api/languages', languagesRoutes);
-app.use('/api/projects', projectsRoutes);
-app.use('/api/project-responsibilities', projectResponsibilitiesRoutes);
-app.use('/api/skills', skillsRoutes);
+
 
 
 // Tăng giới hạn lên 10MB
@@ -78,4 +54,5 @@ db.getConnection((err, connection) => {
         console.log('Kết nối MySQL thành công');
         connection.release();
     }
+
 });
